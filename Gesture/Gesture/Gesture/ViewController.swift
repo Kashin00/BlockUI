cfb5fe8db87d97d9ad8595170526f40cbabeefd0
Matvey <matvexa333@gmail.com>
2021-07-08 12:02:30 +0300

    create proj

---
+++ Gesture/Gesture/Gesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gesture
+//
+//  Created by Матвей Кашин on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
